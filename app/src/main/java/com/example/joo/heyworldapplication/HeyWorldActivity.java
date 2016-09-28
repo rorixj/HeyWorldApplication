9cd63a8b72fa0c1987b17bbc29acb6c8f06981f3
rorix <joao_serra_1@hotmail.com>
2016-09-28 21:34:53 +0100

    Button - code to go to next activity

--- app/src/main/java/com/example/joo/heyworldapplication/HeyWorldActivity.java
+++ app/src/main/java/com/example/joo/heyworldapplication/HeyWorldActivity.java
@@ -1,9 +1,12 @@
 package com.example.joo.heyworldapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class HeyWorldActivity extends AppCompatActivity {
 
@@ -11,7 +14,23 @@ public class HeyWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hey_world);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Do something in response to button click
+                Intent i = new Intent(HeyWorldActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
